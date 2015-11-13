bd2fca5cfbc51564321ccedfb9aae67ca4bc4675
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-11-13 22:43:18 +0000

    fixing tests.

--- test/types.js
+++ test/types.js
@@ -403,3 +403,21 @@ function dateEquals() {
         t.equal(value.toUTCString(), new Date(timestamp).toUTCString());
     }
 }
+
+Buffer.prototype.equals = function (a, b) {
+    if (a === b) {
+        return true;
+    }
+    if (typeof a.equals === 'function') {
+        return a.equals(b);
+    }
+    if (a.length !== b.length) {
+        return false;
+    }
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            return false;
+        }
+    }
+    return true;
+};
