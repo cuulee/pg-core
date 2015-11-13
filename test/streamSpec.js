5c56635274a99265096509c6b53f14db64069eca
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-11-13 02:10:40 +0000

    adding tests.

--- test/streamSpec.js
+++ test/streamSpec.js
@@ -1,12 +1,56 @@
 var shared = require('./db');
 
-var PGResult = require('../lib/result');
-
 var core = shared.core;
 var Promise = shared.promise;
 var db = shared.db;
 
+var QueryStream = core.stream;
+
+function dummy() {
+}
+
 describe("Stream", function () {
+
+    describe("with valid query", function () {
+        var rows = [];
+        beforeEach(function (done) {
+            var ctx;
+            db.connect()
+                .then(function (obj) {
+                    ctx = obj;
+                    return ctx.client.query(new QueryStream("select * from numbers"));
+                })
+                .then(function (stream) {
+                    return new Promise(function (resolve, reject) {
+                        var result = [];
+                        stream.on('data', process);
+                        stream.once('end', function () {
+                            stream.removeListener('data', process);
+                            resolve(result);
+                        });
+                        stream.once('error', function (err) {
+                            reject(err);
+                        });
+                        function process(data) {
+                            result.push(data);
+                        }
+                    });
+                })
+                .then(function (data) {
+                    rows = data;
+                }, dummy)
+                .finally(function () {
+                    if (ctx) {
+                        ctx.done();
+                    }
+                    done();
+                });
+        });
+        it("must be parsed correctly", function () {
+            expect(rows && rows.length === 2).toBe(true);
+            // TODO: need more tests here...
+        });
+    });
 
 });
 
